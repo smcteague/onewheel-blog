47e11f445858596bd7e3319fec6059dc3fa61ef1
smcteague <stevemcteague@gmail.com>
2023-05-23 16:18:51 -0400

    Now using sqlite db.

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -3,29 +3,29 @@ import { LoaderFunction, json } from "@remix-run/node";
 import { getPosts } from "~/models/post.server";
 
 type LoaderData = {
-    posts: Awaited<ReturnType<typeof getPosts>>
-}
+  posts: Awaited<ReturnType<typeof getPosts>>;
+};
 
 export const loader: LoaderFunction = async () => {
-    const posts = await getPosts(); 
-    return json<LoaderData>({ posts });
+  const posts = await getPosts();
+  return json<LoaderData>({ posts });
 };
 
 export default function PostsRoute() {
-    const { posts } = useLoaderData() as LoaderData;
+  const { posts } = useLoaderData() as LoaderData;
 
-    return (
-        <main>
-            <h1>Posts</h1>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.slug}>
-                        <Link to={post.slug} className="text-blue-600 underline">
-                            {post.title}
-                        </Link>        
-                    </li>
-                ))}
-            </ul>
-        </main>
-    )
-};
+  return (
+    <main>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
